70fb8d2790e560f62f36cf2e8e67e976053f39e9
harrishjoshi <dev.harishjoshi@gmail.com>
2025-02-25 12:15:16 +0545

    feat: Make title and content non-nullable

--- FastAPI/blog_api/apps/models/blog.py
+++ FastAPI/blog_api/apps/models/blog.py
@@ -8,5 +8,5 @@ class Blog(Base):
     __tablename__ = "blogs"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    content = Column(String)
+    title = Column(String, index=True, nullable=False)
+    content = Column(String, nullable=False)
