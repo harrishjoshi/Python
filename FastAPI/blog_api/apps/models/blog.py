0757658c76d738ef2a1271d5302ebda706b9db1a
harrishjoshi <dev.harishjoshi@gmail.com>
2025-02-25 13:15:38 +0545

    feat: Add blog created_at and update_at

--- FastAPI/blog_api/apps/models/blog.py
+++ FastAPI/blog_api/apps/models/blog.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -10,3 +12,5 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True, nullable=False)
     content = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.utcnow)
--- FastAPI/blog_api/apps/schemas/blog.py
+++ FastAPI/blog_api/apps/schemas/blog.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pydantic import BaseModel, field_validator
 
 
@@ -32,6 +34,9 @@ class BlogUpdate(BlogBase, PayloadValidation):
 
 class BlogResponse(BlogBase):
     id: int
+    created_at: datetime
+    updated_at: datetime
 
     class Config:
         from_attributes = True
+        arbitrary_types_allowed = True
